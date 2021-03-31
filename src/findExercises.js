4893448770266a5d71f8fc41f8b991224f254cd8
Sidney Crandall <sdncrandall5@gmail.com>
2021-03-31 11:06:55 -0500

    finished find(). reviewing code.

--- src/findExercises.js
+++ src/findExercises.js
@@ -35,9 +35,9 @@ export const getInstructorById = (id) => {
 // It should NOT be case sensitive
 // Ex: getStudentByName("sMiTh")
 export const getStudentByLastName = (lastName) => {
-    const studentLastName = students.find(lastStudent => {
-        if (lastStudent.lastName === lastName) {
-            return lastStudent
+    const studentLastName = students.find(student => {
+        if (student.lastName.toLowerCase() === lastName.toLowerCase()) {
+            return student
         }
     })
     return studentLastName
@@ -48,15 +48,35 @@ export const getStudentByLastName = (lastName) => {
 // It should return the student object whose first and last name match `fullName`
 // It should NOT be case sensitive
 // Ex: getStudentByName("Summer SMITH")
+export const getStudentByName = (fullName) => {
+    const studentName = students.find(studnet =>
+        `${studnet.firstName} ${studnet.lastName}`.toLowerCase() === fullName.toLowerCase()
+    )
+    return studentName
+}
 
 // Export a function called getInstructorOfStudent
 // It should accept one integeter parameter named `studentId`
 // It should return the instructor object of the student whose id matches `studentId`
 // Ex: getInstructorOfStudent(4)      // returns Brenda Long
-
-
+export const getInstructorOfStudent = (studentId) => {
+    const instructor = students.find(studnet => studentId === studnet.id) 
+        return instructors.find(senpai => senpai.id === instructor.instructorId)
+}
+  
 // Export a function called getStudentWithMostLangs
 // It should not accept any parameters
 // It should return the student object who knows the most programming languages
 // Ex: getStudentWithMostLangs()      // returns Rick Sanchez
 // HINT: You may not need the `find` method for this. This is one of the few cases where a `for` loop might be appropriate
+export const getStudentWithMostLangs =  () =>{
+    let mostLang = 0;
+    let length = 0;
+        students.forEach(code => {
+            if(code.languages.length > mostLang) {
+                mostLang = code.languages.length
+                length = code.id
+            }
+        })
+        return students.find(student => student.id === length)
+}
